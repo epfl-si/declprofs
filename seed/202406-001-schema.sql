427300e052ae6d29d39128f3e955379bcba0fb7e
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-06-19 12:13:23 +0200

    [feature] Containerized development environment
    - Made up of `docker-compose.yml`, companion `.env`, and bare-bones `Makefile` that provides `make up logs`
    - Run the production container with a mounted volume for the CGIs
    - Hermetic (albeit empty) database

---
+++ seed/202406-001-schema.sql
@@ -0,0 +1,136 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for macos14.2 (x86_64)
+--
+-- Host: 127.0.0.1    Database: declprofs
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.16-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logs` (
+  `ts` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `persid` char(6) NOT NULL,
+  `doneby` char(6) NOT NULL,
+  `descr` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci AVG_ROW_LENGTH=47;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logs`
+--
+
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profsacts`
+--
+
+DROP TABLE IF EXISTS `profsacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `profsacts` (
+  `year` char(4) DEFAULT NULL,
+  `ordre` tinyint(4) DEFAULT NULL,
+  `persid` char(6) DEFAULT NULL,
+  `activity` varchar(512) DEFAULT NULL,
+  `principal` varchar(512) DEFAULT NULL,
+  `ts` datetime DEFAULT NULL,
+  `status` char(1) DEFAULT NULL,
+  `doneby` char(6) DEFAULT NULL,
+  KEY `persid` (`persid`,`year`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci AVG_ROW_LENGTH=124;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profsacts`
+--
+
+LOCK TABLES `profsacts` WRITE;
+/*!40000 ALTER TABLE `profsacts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profsacts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `remarks`
+--
+
+DROP TABLE IF EXISTS `remarks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `remarks` (
+  `year` char(4) DEFAULT NULL,
+  `ordre` tinyint(4) DEFAULT NULL,
+  `persid` char(6) DEFAULT NULL,
+  `remark` varchar(512) DEFAULT NULL,
+  `ts` datetime DEFAULT NULL,
+  `doneby` char(6) DEFAULT NULL,
+  `prof_remark` varchar(512) DEFAULT NULL,
+  KEY `persid` (`persid`,`year`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci AVG_ROW_LENGTH=67;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `remarks`
+--
+
+LOCK TABLES `remarks` WRITE;
+/*!40000 ALTER TABLE `remarks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `remarks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reminders`
+--
+
+DROP TABLE IF EXISTS `reminders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reminders` (
+  `year` char(4) NOT NULL,
+  `persid` varchar(6) DEFAULT NULL,
+  `reminddate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `nxtdelay` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci AVG_ROW_LENGTH=24;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reminders`
+--
+
+LOCK TABLES `reminders` WRITE;
+/*!40000 ALTER TABLE `reminders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reminders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'declprofs'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-19 12:00:53
